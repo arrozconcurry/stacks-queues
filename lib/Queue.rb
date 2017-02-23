8b953468fbfdfa67d796ecbb4bda05eaea7c1fa6
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-02-23 12:24:07 -0800

    2.23 queue code

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store_q = Array.new
   end
 
   def enqueue(element)
+    @store_q.push(element)
   end
 
   def dequeue
+    @store_q.shift
   end
 
   def front
+    return @store_q.first
   end
 
   def size
+    return @store_q.length
   end
 
   def empty?
+    return @store_q.empty?
   end
 
   def to_s
+    return @store_q.to_s
   end
 end
